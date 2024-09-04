075f62d46914097ee7ce376df665fe6c82cb9400
Abram <abramhtran@gmail.com>
2024-09-04 11:05:54 -0700

    yes

--- problem1.java
+++ problem1.java
@@ -1,1 +1,11 @@
 // Create your class here; don't forget the main method
+public class problem1 
+{
+    public static void main(String[] args)
+    {
+        String food;
+        food = "bananas";
+
+        System.out.print("I like to eat " + food + " as well!");
+    }
+}
