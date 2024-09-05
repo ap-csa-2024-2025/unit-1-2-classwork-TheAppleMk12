efdea6aa409327db3d67d221561c5c32b4b1f7eb
Abram <Abramhtran@gmail.com>
2024-09-04 23:32:11 -0700

    Completed

--- problem2.java
+++ problem2.java
@@ -1,1 +1,20 @@
 // Create your class here; don't forget the main method
+public class problem2 
+{
+    public static void main(String[] args)
+    {
+        String firstName1;
+        firstName1 = "Abram";
+
+        String firstName2;
+        firstName2 = "Nathan";
+
+        String lastName1;
+        lastName1 = "Tran";
+
+        String lastName2;
+        lastName2 = "Dang";
+
+        System.out.print(lastName1+ ", " + firstName1 + "   " + lastName2 + ", " + firstName2);
+    }
+}
--- problem3.java
+++ problem3.java
@@ -1,1 +1,14 @@
 // Create your class here; don't forget the main method
+public class problem3
+{
+    public static void main(String[] args)
+    {
+        String State;
+        State = "California";
+
+        String Name;
+        Name = "Abram";
+
+        System.out.print("My name is " + Name + ". I live in " + State);
+    }
+}
